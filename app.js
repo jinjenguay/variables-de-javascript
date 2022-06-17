061d40db78178b25138fc435008f671499c06eaf
jinjenguay <jdemarazzi@gmail.com>
2022-06-16 21:08:46 -0300

    variables

---
+++ app.js
@@ -0,0 +1,77 @@
+// palabra reservada, nombre, asignacion y el valor;
+
+//const edad = 25;
+
+ // declaracion multiple
+
+//const edad = 25, nombre = 'john', altura = 1.85
+
+
+// string
+
+const nombre = 'john';
+const apellido = 'parra';
+const compañera = 'hannah';
+const compañera2 = 'caro';
+
+// number
+
+const edad = 25;
+const altura = 1.86;
+const negativo = -10;
+
+
+// boolean
+
+const estudiante = true;
+const esProfe = false; 
+
+// underfined
+
+const indefinida = undefined;
+
+// null
+
+const noExiste = null;
+
+//console.log('holiwis che');
+
+console.log(nombre);
+
+//typeof nos dice qué tipo de variable es la que nombramos por su nombre a continuación:
+
+console.log(typeof nombre)
+
+const ejemplo1 = '2';
+console.log('2' + 1); //no va aa funcionar, me los va a concatenar, unir.
+
+console.log( Number(2) + 1)
+
+// prompt me da una ventana emergente con el texto que yo elija y una caja para rellenar, tal vez en respuesta al texto:
+//const primerNumero = prompt ('ingresa tal');
+//const segundoNumero = prompt('ingresar aquello');
+
+//console.log(primerNumero + segundoNumero); // como es tipo de dato string los va a unir y no sumar
+ 
+//de esta manera hago que sepa que son numeros, y que debe sumarlos
+//console.log(Number(primerNumero) + Number (segundoNumero));
+
+
+//Aca aclaro que el numero entre parenesis es un texto, no un numero:
+console.log(String(2));
+
+//Acá vuelvo un string/un texto, en Boolean. cohercion de datos forzada
+console.log(Boolean('true'))
+
+//concatenación, ejemplo. 
+
+//const nombreUsuario = prompt('Ingresa tu nombre')
+
+//alert('Hola ' + nombreUsuario + ' bienvenida')
+
+console.warn('advertencia');
+
+console.error('Error con esto y lo otro');
+
+
+
